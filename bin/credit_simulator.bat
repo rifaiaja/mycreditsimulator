e0484d8c3735ae640437626eafa1b7db02aa31aa
Wahyu Rifai <wahyu.rds@ukuindo.com>
2025-08-26 15:09:45 +0700

    Add file executable

---
+++ bin/credit_simulator.bat
@@ -0,0 +1,11 @@
+@echo off
+set JAR=%~dp0\original-credit-simulator-1.0.0-shaded.jar
+if not exist "%JAR%" (
+  echo JAR not found. Please run: mvn package
+  exit /b 1
+)
+if "%~1"=="" (
+  java -jar "%JAR%"
+) else (
+  java -jar "%JAR%" "%~1"
+)
